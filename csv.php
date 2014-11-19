8861cdfe6b5f3b33b95bc3501f65b907a35067ab
croxxx <i.am.croxxx@aol.de>
2014-11-19 18:03:24 +0100

    Make csv.php actually output the server list

--- csv.php
+++ csv.php
@@ -10,16 +10,16 @@ function GenerateCSV() {
 
 	foreach($servers as $s) {
 		//Write server values into a line (separated by tabs)
-		$output.$s->getPublicKey()."\t";
-		$output.$s->getName()."\t";
-		$output.$s->getMotd()."\t";
-		$output.$s->getPort()."\t";
-		$output.$s->getAddress()."\t";
-		$output.$s->getVersion()."\t";
-		$output.$s->getUserCount()."\t";
-		$output.$s->getMaxClients()."\t";
-		$output.$s->getGameMode()."\t";
-		$output.$s->getUserList()."\n";	//Terminate server entry using line break
+		$output.=$s->getPublicKey()."\t";
+		$output.=$s->getName()."\t";
+		$output.=$s->getMotd()."\t";
+		$output.=$s->getPort()."\t";
+		$output.=$s->getAddress()."\t";
+		$output.=$s->getVersion()."\t";
+		$output.=$s->getUserCount()."\t";
+		$output.=$s->getMaxClients()."\t";
+		$output.=$s->getGameMode()."\t";
+		$output.=$s->getUserList()."\n";	//Terminate server entry using line break
 	}
 	return $output;
 }
