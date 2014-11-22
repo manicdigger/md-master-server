457202aef3176699430a5abb089c8a3c2a67fd2d
croxxx <i.am.croxxx@aol.de>
2014-11-22 01:43:57 +0100

    Fixed update account dialog not showing. Replicate layout from previous version.

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ if(isset($_SESSION["username"])) {
 		<div class="row">
 			<div class="col-md-6"><img src="img/header.png" /></div>
 			<div class="col-md-6">
+				<!-- Top right navbar -->
 				<div class="login-box">
 					<div id="logged-out" class="<?php if($loggedIn) { echo "hide"; }?>">
 						<form id="login" action="loginuser.php" method="post" accept-charset="UTF-8" class="form-inline login-form" role="form">
@@ -48,12 +49,14 @@ if(isset($_SESSION["username"])) {
 					<div id="logged-in" class="<?php if(!$loggedIn) { echo "hide"; }?>">
 						Logged in as <span id="username-text"><?php echo $username ?></span> 
 						<span class="pull-right">
+							<a href="#change-skin-modal" data-toggle="modal">Change Skin</a> | 
 							<a href="#update-account-modal" data-toggle="modal">Update Account</a> | 
 							<a id="logout-link" href="logoutuser.php">Logout</a>
 						</span>
 					</div>
 				</div>
 				
+				<!-- Modal dialogs -->
 				<div id="create-account-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-create" aria-hidden="true">
 					<div class="modal-dialog">
 						<div class="modal-content">
@@ -64,29 +67,32 @@ if(isset($_SESSION["username"])) {
 								</div>
 								<div class="modal-body">
 									<div class="form-group">
-										<label for="inputEmail" class="col-md-3">Username</label>
-										<div class="col-md-6">
-											<input class="form-control" type="text" name="username" id="create-username" tabindex="30" placeholder="Username" required><span class="help-inline"><span id="username-check" class="hide label"></span></span>
+										<label for="inputEmail" class="col-md-4 control-label">Username</label>
+										<div class="col-md-5">
+											<input class="form-control" type="text" name="username" id="create-username" tabindex="30" placeholder="Username" required>
+										</div>
+										<div class="col-md-3">
+											<span id="username-check" class="hide label"></span>
 										</div>
 									</div>
 									<div class="form-group">
-										<label for="create-password" class="col-md-3">Password</label>
-										<div class="col-md-6">
+										<label for="create-password" class="col-md-4 control-label">Password</label>
+										<div class="col-md-5">
 											<input class="form-control" type="password" name="password" id="create-password" tabindex="31" placeholder="Password" required>
 										</div>
-										<div class="col-md-2">
-											<span class="help-inline"><span id="password-check" class="hide label"></span></span>
+										<div class="col-md-3">
+											<span id="password-check" class="hide label"></span>
 										</div>
-										<div class="col-md-offset-3 col-md-6">
+										<div class="col-md-offset-4 col-md-5">
 											<input class="form-control" type="password" name="passwordagain" id="create-password-again" tabindex="32" placeholder="Password Again" required>
 										</div>
-										<div class="col-md-2">
-											<span class="help-inline"><span id="password-match" class="hide label"></span></span>
+										<div class="col-md-3">
+											<span id="password-match" class="hide label"></span>
 										</div>
 									</div>
 									<div class="form-group">
-										<label for="create-email" class="col-md-3">Email</label>
-										<div class="col-md-6">
+										<label for="create-email" class="col-md-4 control-label">Email</label>
+										<div class="col-md-5">
 											<input class="form-control" type="text" name="email" id="create-email" tabindex="33" placeholder="Email (optional)">
 										</div>
 									</div>
@@ -100,40 +106,51 @@ if(isset($_SESSION["username"])) {
 					</div>
 				</div>
 				
-				
-				<form id="update-account" class="form-horizontal">
-					<div id="update-account-modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="modal-update" aria-hidden="true">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<h3 id="modal-update">Update Account</h3>
-						</div>
-						<div class="modal-body">
-							<div class="control-group">
-								<label class="control-label" for="update-password">Password</label>
-								<div class="controls">
-									<input type="password" name="password" id="update-password" tabindex="31" placeholder="Password" required>
+				<div id="update-account-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-update" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<form id="update-account" class="form-horizontal">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									<h3 id="modal-update">Update Account</h3>
 								</div>
-							</div>
-							<div class="control-group">
-								<label class="control-label" for="update-password">New Password</label>
-								<div class="controls">
-									<input type="password" name="newpassword" id="update-new-password" tabindex="31" placeholder="Password (Only if new)"><span class="help-inline"><span id="update-password-check" class="hide label"></span></span>
-									<input type="password" name="newpasswordagain" id="update-new-password-again" tabindex="32" placeholder="Password Again (Only if new)"><span class="help-inline"><span id="update-password-match" class="hide label"></span></span>
+								<div class="modal-body">
+									<div class="form-group">
+										<label class="col-md-4 control-label" for="update-password">Password</label>
+										<div class="col-md-5">
+											<input class="form-control" type="password" name="password" id="update-password" tabindex="31" placeholder="Password" required>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-4 control-label" for="update-password">New Password</label>
+										<div class="col-md-5">
+											<input class="form-control" type="password" name="newpassword" id="update-new-password" tabindex="31" placeholder="Password (Only if new)">
+										</div>
+										<div class="col-md-3">
+											<span id="update-password-check" class="hide label"></span>
+										</div>
+										<div class="col-md-offset-4 col-md-5">
+											<input class="form-control" type="password" name="newpasswordagain" id="update-new-password-again" tabindex="32" placeholder="Password Again (Only if new)">
+										</div>
+										<div class="col-md-3">
+											<span id="update-password-match" class="hide label"></span>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-4 control-label" for="update-email">Email</label>
+										<div class="col-md-5">
+											<input class="form-control" type="text" name="email" id="update-email" tabindex="33" placeholder="Email (optional)">
+										</div>
+									</div>
 								</div>
-							</div>
-							<div class="control-group">
-								<label class="control-label" for="update-email">Email</label>
-								<div class="controls">
-									<input type="text" name="email" id="update-email" tabindex="33" placeholder="Email (optional)">
+								<div class="modal-footer">
+									<button tabindex="40" class="btn btn-default" data-dismiss="modal" aria-hidden="true">Close</button>
+									<button tabindex="35" class="btn btn-primary" type="submit">Update Account</button>
 								</div>
-							</div>
-						</div>
-						<div class="modal-footer">
-							<button tabindex="40" class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-							<button tabindex="35" class="btn btn-primary" type="submit">Update Account</button>
+							</form>
 						</div>
 					</div>
-				</form>
+				</div>
 				
 				<!--This height needs to be slightly less than the header.png-->
 				<div style="position:relative; height:130px;">
